31b318b18f6aea43309c9b901ff9b4bf55be6a43
Gabriel <gkime91@gmail.com>
2024-02-18 12:57:32 -0800

    creating, running bash script. variables, echo, input

---
+++ bashtut1.sh
@@ -0,0 +1,17 @@
+# shebang!
+#!/bin/bash
+# chmod +x /bashtut1.sh to make executable
+echo "Welcome To Bash!"
+# run in commmand line
+
+# VARIABLES
+# name="Gabriel"
+# $$$$$$
+# echo "Hello, $name"
+
+# USER INPUT
+echo "Please enter your username"
+# ...
+# wait for user input
+read username
+echo "Hello, $username"
